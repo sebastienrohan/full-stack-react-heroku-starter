18bb54284b5d8694025916de59ed5ded5ec776e3
Sébastien Rohan <rohan.sebastien@gmail.com>
2017-07-04 12:57:27 +0200

    Added start-dev script & MONGODB_URI

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // connect to the database and load models
-require('./models').connect('mongodb://localhost/react_app')
+require('./models').connect(process.env.MONGODB_URI)
 
 const app = express()
 const PORT = process.env.PORT || 5000
